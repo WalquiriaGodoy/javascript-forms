74d1d9d59de1e4470e695c4a73d50ee583e72806
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-12-03 18:06:12 -0300

    feat(form): Add function that validates cpf

--- js/validacao.js
+++ js/validacao.js
@@ -35,11 +35,16 @@ const mensagensDeErro = {
     dataNascimento:{
         valueMissing: 'O campo data de nascimento não pode estar vazio',
         customError: 'Você deve ser maior de 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio',
+        customError: 'O CPF digitado não é válido '
     }
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input){
@@ -72,3 +77,80 @@ function maiorQue18(data) {
     
     return dataMais18 <= dataAtual
 }
+
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if (!checaCPFRepetido(cpfFormatado)|| !cpfEhValido(cpfFormatado)){
+        mensagem = 'O CPF digitado não é válido'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf){
+            cpfValido = false
+        }
+        
+    })
+    return cpfValido
+
+}
+
+function cpfEhValido(cpf){
+        let Soma;
+        let Resto;
+        Soma = 0;
+
+        if (cpf == "00000000000") return false;
+
+        for (let i = 1; i <=9; i++){
+            Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+        }
+
+        Resto = (Soma * 10) % 11;// % ou mod, retorna o resto da divisão
+
+        if ((Resto == 10) || (Resto == 11)){ 
+            Resto = 0;
+        } 
+
+        if (Resto != parseInt(cpf.substring(9, 10)) ){
+            return false;
+        }
+
+        Soma = 0;
+        
+        for (let i = 1; i <= 10; i++){
+            Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+        }
+
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11)){
+            Resto = 0;
+        } 
+
+        if (Resto != parseInt(cpf.substring(10, 11) ) ) {
+            return false;
+        }
+
+        return true;
+    }
